fc4b3dacc859de6ef3c79dd000b283fadd68f078
karasinga <mainpita@gmail.com>
2023-03-06 21:44:21 +0300

    chore: handle database exception and update login view

--- apps/pmtct/admin.py
+++ apps/pmtct/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.pmtct.models import PatientDetails, RiskCategorization, RiskCategorizationTrial
+from apps.pmtct.models import PatientDetails, RiskCategorizationTrial
 
-admin.site.register(RiskCategorization)
+# admin.site.register(RiskCategorization)
 admin.site.register(PatientDetails)
 admin.site.register(RiskCategorizationTrial)
--- apps/pmtct/form.py
+++ apps/pmtct/form.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 
-from apps.pmtct.models import PatientDetails, RiskCategorization, RiskCategorizationTrial
+from apps.pmtct.models import PatientDetails,  RiskCategorizationTrial
 from apps.cqi.models import Facilities
 
 
@@ -17,22 +17,22 @@ class PatientDetailsForm(ModelForm):
         exclude = ['age', 'edd', 'gestation_by_age']
 
 
-class RiskCategorizationForm(ModelForm):
-    pmtct_mother = forms.ModelChoiceField(queryset=PatientDetails.objects.all(), required=False)
-
-    class Meta:
-        model = RiskCategorization
-        fields = "__all__"
-        exclude = ['id']
-
-    # This is the constructor method of the form
-    def __init__(self, *args, **kwargs):
-        # Call the parent constructor method
-        super().__init__(*args, **kwargs)
-        # Loop through all the fields in the form
-        for field in self.fields:
-            # Set the label of each field to False
-            self.fields[field].label = False
+# class RiskCategorizationForm(ModelForm):
+#     pmtct_mother = forms.ModelChoiceField(queryset=PatientDetails.objects.all(), required=False)
+#
+#     class Meta:
+#         model = RiskCategorization
+#         fields = "__all__"
+#         exclude = ['id']
+#
+#     # This is the constructor method of the form
+#     def __init__(self, *args, **kwargs):
+#         # Call the parent constructor method
+#         super().__init__(*args, **kwargs)
+#         # Loop through all the fields in the form
+#         for field in self.fields:
+#             # Set the label of each field to False
+#             self.fields[field].label = False
 
 
 class RiskCategorizationTrialForm(ModelForm):

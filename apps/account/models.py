ff766958e2ae8533c6af33e528b2e4ca0d5e52c3
karasinga <mainpita@gmail.com>
2023-03-07 09:57:32 +0300

    fix: fix FK errors

--- apps/account/models.py
+++ apps/account/models.py
@@ -9,7 +9,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomUser(AbstractUser):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     phone_number = PhoneNumberField(null=True, blank=True)
 
     class Meta:

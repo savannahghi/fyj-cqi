65a1afcb5291d47292399cd94e3fc18cd67c02dd
karasinga <76413541+karasinga@users.noreply.github.com>
2023-12-19 11:47:10 +0300

    Release: (#169)
    * fix: download cd4 count result (#135)
    
    * fix:strip-off password/username leading and trailing spaces (#137)
    
    * fix:CQI model ordering and enhance admin site return strings (#139)
    
    * fix:enhance reading drt pdf (#141)
    
    * fix:reading PI and INSTIs mutations (#143)
    
    * fix: extracting date from text (#145)
    
    * fix: Validate form names (#147)
    
    * fix: order age bands chronologically (#149)
    
    * fix: dqa charts categorical values (#151)
    
    * fix:Download csv keyerror and transcription errors on DRT (#153)
    
    * fix:Upload and save failed DRT results (#155)
    
    * fix: handle patient unique id (#156)
    
    * resolve conflicts
    
    * fix: (#158)
    
    * fix:Access dqa_dashboard/{dqa-type} db error (#160)
    
    * fix:refining line joining logic (#162)
    
    * fix download csv button session error (#164)
    
    * Fix: Prevent double submission of the login page (#166)
    
    * Addressing N+1 Query Issue in DQA Dashboard View (#168)

--- apps/dqa/urls.py
+++ apps/dqa/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import include, path
 
-from apps.dqa.views import add_data_verification, add_period, show_data_verification, update_data_verification, \
-    delete_data_verification, load_data, dqa_summary, dqa_work_plan_create, show_dqa_work_plan, load_system_data, \
-    add_system_verification, system_assessment_table, instructions, update_system_assessment, update_dqa_workplan, \
-    GeneratePDF, add_audit_team, update_audit_team, show_audit_team, load_khis_data, update_button_settings, \
-    dqa_dashboard, export_dqa_work_plan_csv
+from apps.dqa.views import GeneratePDF, add_audit_team, add_data_verification, add_period, add_system_verification, \
+    delete_data_verification, dqa_dashboard, dqa_summary, dqa_work_plan_create, export_dqa_work_plan_csv, instructions, \
+    load_data, load_khis_data, load_system_data, show_audit_team, show_data_verification, show_dqa_work_plan, \
+    system_assessment_table, update_audit_team, update_button_settings, update_data_verification, update_dqa_workplan, \
+    update_system_assessment
 
 urlpatterns = [
-    path('add-data-verification/', add_data_verification,name="add_data_verification"),
-    path('add-system-verification/', add_system_verification,name="add_system_verification"),
-    path('add-audit-team/<uuid:pk>/<str:quarter_year>', add_audit_team,name="add_audit_team"),
-    path('choose-period/', add_period,name="add_period"),
-    path('show-data-verification/', show_data_verification,name="show_data_verification"),
+    path('add-data-verification/', add_data_verification, name="add_data_verification"),
+    path('add-system-verification/', add_system_verification, name="add_system_verification"),
+    path('add-audit-team/<uuid:pk>/<str:quarter_year>', add_audit_team, name="add_audit_team"),
+    path('choose-period/', add_period, name="add_period"),
+    path('show-data-verification/', show_data_verification, name="show_data_verification"),
     path('dqa/datim-load-data/', load_data, name='load_datim_data'),
     path('dqa/khis-load-data/', load_khis_data, name='load_khis_data'),
     path('dqa/load_system_data/', load_system_data, name='load_system_data'),
@@ -26,13 +27,20 @@ urlpatterns = [
     path('audit-team/', show_audit_team, name='show_audit_team'),
     path('dqa-dashboard/<str:dqa_type>', dqa_dashboard, name='dqa_dashboard'),
 
-    path('update-data-verification/<uuid:pk>', update_data_verification,name="update_data_verification"),
-    path('update-system-assessment/<uuid:pk>', update_system_assessment,name="update_system_assessment"),
-    path('update-audit-team/<uuid:pk>', update_audit_team,name="update_audit_team"),
-    path('update-dqa-workplan/<uuid:pk>', update_dqa_workplan,name="update_dqa_workplan"),
-    path('delete-data-verification/<uuid:pk>', delete_data_verification,name="delete_data_verification"),
+    path('update-data-verification/<uuid:pk>', update_data_verification, name="update_data_verification"),
+    path('update-system-assessment/<uuid:pk>', update_system_assessment, name="update_system_assessment"),
+    path('update-audit-team/<uuid:pk>', update_audit_team, name="update_audit_team"),
+    path('update-dqa-workplan/<uuid:pk>', update_dqa_workplan, name="update_dqa_workplan"),
+    path('delete-data-verification/<uuid:pk>', delete_data_verification, name="delete_data_verification"),
     # path('dqa_summary_pdf/', generate_pdf, name='dqa_summary_pdf'),
     path('generate-pdf/', GeneratePDF.as_view(), name='generate-pdf'),
-    path('download-dqa-workplan/<str:quarter_year>/<str:selected_level>/', export_dqa_work_plan_csv, name='download_dqa_workplan'),
+    path('download-dqa-workplan/<str:quarter_year>/<str:selected_level>/', export_dqa_work_plan_csv,
+         name='download_dqa_workplan'),
 
 ]
+# Include debug toolbar URLs only in DEBUG mode
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ]

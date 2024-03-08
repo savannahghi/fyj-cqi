431fbc8d2c214defa012109d8b6e1985d1be4de2
karasinga <76413541+karasinga@users.noreply.github.com>
2024-03-08 12:19:18 +0300

    New: Add RTK model and analysis (#239)

---
+++ apps/data_analysis/filters.py
@@ -0,0 +1,110 @@
+import django_filters
+from django import forms
+from django_filters import CharFilter, DateFilter, NumberFilter
+
+from apps.cqi.models import Counties, Facilities, Sub_counties
+from apps.data_analysis.models import RTKData
+
+
+# class RTKDataFilter(django_filters.FilterSet):
+#     start_date = DateFilter(field_name="month_column", lookup_expr="gte", label='From (Reporting Month)')
+#     end_date = DateFilter(field_name="month_column", lookup_expr="lte", label='To (Reporting Month)')
+#
+#     class Meta:
+#         model = RTKData
+#         fields = "__all__"
+#
+#     sub_county = django_filters.ChoiceFilter(choices=[], label='Sub-County',
+#                                              widget=forms.Select(attrs={'class': 'form-control select2'}))
+#     commodity_name = django_filters.ChoiceFilter(choices=[], label='Commodity Type',
+#                                              widget=forms.Select(attrs={'class': 'form-control select2'}))
+#     county = django_filters.ChoiceFilter(choices=[], label='County',
+#                                          widget=forms.Select(attrs={'class': 'form-control select2'}))
+#     facility_name = django_filters.ChoiceFilter(choices=[], label='Facility Name',
+#                                              widget=forms.Select(attrs={'class': 'form-control select2'}))
+#     month = django_filters.ChoiceFilter(choices=[], label='Month-Year',
+#                                                 widget=forms.Select(attrs={'class': 'form-control select2'}))
+#
+#     def __init__(self, *args, **kwargs):
+#         """
+#         Custom initialization method for the BiochemistryResultFilter.
+#
+#         Parameters:
+#             *args (tuple): Positional arguments.
+#             **kwargs (dict): Keyword arguments.
+#
+#         This method is called when an instance of BiochemistryResultFilter is created.
+#         It retrieves unique 'test' and 'test interpretation' values from the BiochemistryResult model
+#         and sets them as choices for the corresponding filters.
+#
+#         The 'test' choices are set for the 'full_name' filter.
+#         The 'test interpretation' choices are set for the 'results_interpretation' filter.
+#         """
+#         super().__init__(*args, **kwargs)
+#
+#         # Get unique 'test' values from the database
+#         unique_tests = RTKData.objects.values_list('facility_name', flat=True).distinct()
+#
+#         # Create the choices for the 'test' field
+#         test_unit_choices = set([(test, test) for test in unique_tests])
+#         self.filters['facility_name'].extra['choices'] = sorted(test_unit_choices)
+#
+#         # Get unique 'test' values from the database
+#         unique_tests = RTKData.objects.values_list('sub_county', flat=True).distinct()
+#
+#         # Create the choices for the 'test' field
+#         test_unit_choices = set([(test, test) for test in unique_tests])
+#         self.filters['sub_county'].extra['choices'] = sorted(test_unit_choices)
+#
+#         # Get unique 'test' values from the database
+#         unique_tests = RTKData.objects.values_list('county', flat=True).distinct()
+#
+#         # Create the choices for the 'test' field
+#         test_unit_choices = set([(test, test) for test in unique_tests])
+#         self.filters['county'].extra['choices'] = sorted(test_unit_choices)
+#
+#         # Get unique 'test' values from the database
+#         unique_tests = RTKData.objects.values_list('commodity_name', flat=True).distinct()
+#
+#         # Create the choices for the 'test' field
+#         test_unit_choices = set([(test, test) for test in unique_tests])
+#         self.filters['commodity_name'].extra['choices'] = sorted(test_unit_choices)
+#
+#         # Get unique 'test' values from the database
+#         unique_tests = RTKData.objects.values_list('month', flat=True).distinct()
+#
+#         # Create the choices for the 'test' field
+#         test_unit_choices = set([(test, test) for test in unique_tests])
+#         self.filters['month'].extra['choices'] = sorted(test_unit_choices)
+class RTKDataFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name="month_column", lookup_expr="gte", label='From (Reporting Month)')
+    end_date = DateFilter(field_name="month_column", lookup_expr="lte", label='To (Reporting Month)')
+
+    class Meta:
+        model = RTKData
+        fields = "__all__"
+
+    sub_county = django_filters.MultipleChoiceFilter(choices=[], label='Sub-County',
+                                                         widget=forms.SelectMultiple(
+                                                             attrs={'class': 'form-control select2'}))
+
+    commodity_name = django_filters.MultipleChoiceFilter(choices=[], label='Commodity Type',
+                                                         widget=forms.SelectMultiple(
+                                                             attrs={'class': 'form-control select2'}))
+    county = django_filters.MultipleChoiceFilter(choices=[], label='County',
+                                                         widget=forms.SelectMultiple(
+                                                             attrs={'class': 'form-control select2'}))
+    facility_name = django_filters.ChoiceFilter(choices=[], label='Facility Name',
+                                                widget=forms.Select(attrs={'class': 'form-control select2'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Get unique values from the database for all fields in a single query
+        unique_values = RTKData.objects.values('facility_name', 'sub_county', 'county', 'commodity_name',
+                                               ).distinct()
+
+        # Create the choices for each field
+        for field_name in ['facility_name', 'sub_county', 'county', 'commodity_name']:
+            test_unit_choices = set([(item[field_name], item[field_name]) for item in unique_values])
+            self.filters[field_name].extra['choices'] = sorted(test_unit_choices)
--- apps/data_analysis/models.py
+++ apps/data_analysis/models.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
 
 from django.db import models
+
+from apps.labpulse.models import BaseModel
 
 
 class FYJHealthFacility(models.Model):
@@ -30,4 +34,31 @@ class FYJHealthFacility(models.Model):
 
     def __str__(self):
         return str(self.facility) + "-" + str(self.mfl_code)
+
+class RTKData(BaseModel):
+    month = models.CharField(max_length=10)
+    county = models.CharField(max_length=50)
+    sub_county = models.CharField(max_length=50)
+    mfl_code = models.IntegerField()
+    facility_name = models.CharField(max_length=250)
+    commodity_name = models.CharField(max_length=50)
+    beginning_balance = models.IntegerField()
+    quantity_received = models.IntegerField()
+    quantity_used = models.IntegerField()
+    quantity_requested = models.IntegerField()
+    tests_done = models.IntegerField()
+    losses = models.IntegerField()
+    positive_adjustments = models.IntegerField()
+    negative_adjustments = models.IntegerField()
+    ending_balance = models.IntegerField()
+    days_out_of_stock = models.IntegerField()
+    quantity_expiring_in_6_months = models.IntegerField()
+    month_column = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        unique_together = (('mfl_code', 'month','commodity_name'),)
+        ordering=["facility_name","commodity_name"]
+
+    def __str__(self):
+        return f"{self.facility_name} - {self.month} - {self.commodity_name}"
 

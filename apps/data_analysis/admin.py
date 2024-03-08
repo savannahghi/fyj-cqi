40bd45f71dba3a79c48dde8042b18890abd79f57
karasinga <76413541+karasinga@users.noreply.github.com>
2024-03-08 13:04:52 +0300

    Release: (#241)
    * fix: download cd4 count result (#135)
    
    * fix:strip-off password/username leading and trailing spaces (#137)
    
    * fix:CQI model ordering and enhance admin site return strings (#139)
    
    * fix:enhance reading drt pdf (#141)
    
    * fix:reading PI and INSTIs mutations (#143)
    
    * fix: extracting date from text (#145)
    
    * fix: Validate form names (#147)
    
    * fix: order age bands chronologically (#149)
    
    * fix: dqa charts categorical values (#151)
    
    * fix:Download csv keyerror and transcription errors on DRT (#153)
    
    * fix:Upload and save failed DRT results (#155)
    
    * fix: handle patient unique id (#156)
    
    * resolve conflicts
    
    * fix: (#158)
    
    * fix:Access dqa_dashboard/{dqa-type} db error (#160)
    
    * fix:refining line joining logic (#162)
    
    * fix download csv button session error (#164)
    
    * Fix: Prevent double submission of the login page (#166)
    
    * Addressing N+1 Query Issue in DQA Dashboard View (#168)
    
    * Feat: Add DrtProfile model (#170)
    
    * add_drt_cascade (#172)
    
    * fix: ValueError: No objects to concatenate (#174)
    
    * fix: N+1 Query (#176)
    
    * Feat: add Patient-wise Distribution of Resistance Types (#178)
    
    * Fix: Make DRT chart titles descriptive (#180)
    
    * fix: handle keyword in gender filters (#182)
    
    * Fix: keyerror for image type (#184)
    
    * Fix: N+1 Query (#186)
    
    * Fix: Enhance DQA worplan pages (#188)
    
    * fix:  Handling Turnaround Time Calculation in Cd4traker Model (#190)
    
    * Fix:optimize_show_results (#192)
    
    * Fix: Include all download buttons (#193)
    
    * Fix: correct filtering (#195)
    
    * Fix:Multifile upload (#197)
    
    * Fix: Read stanford pdf text errors (#199)
    
    * Fix: Displaying Serum CRAG results (#201)
    
    * fix:add county and subcounty columns DAC (#203)
    
    * Fix:pharmacy dqa (#205)
    
    * Fix:Add multiple upload capabilities (#207)
    
    * Fix: pharmacy DQA scoring (#209)
    
    * Fix: Facility mean score (#211)
    
    * Fix_500_error (#213)
    
    * Fix: Facility scoring (#215)
    
    * Fix: Percentage on the facility scoring (#217)
    
    * Add: Introduce Repository app (#219)
    
    * Fix: include ckeditor js (#221)
    
    * Fix: Update biochem section (#223)
    
    * Fix: biochem charts (#225)
    
    * Fix: Add facility, county and subcounty foreignkeys (#227)
    
    * Fix: Add facility, county and subcounty foreignkeys (#228)
    
    * Add: Add TAT days (#230)
    
    * fix: chart error (#231)
    
    * Fix: Improved Filtering Choices for Biochemistry Results (#233)
    
    * Fix: Download biochem results (#235)
    
    * Fix: Handle missing facilities in the uploaded dataset (#237)
    
    * New: Add RTK model and analysis (#239)
    
    * fix: add upload hcmp report url (#240)

--- apps/data_analysis/admin.py
+++ apps/data_analysis/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.data_analysis.models import FYJHealthFacility
+from apps.data_analysis.models import FYJHealthFacility, RTKData
 
 admin.site.register(FYJHealthFacility)
+admin.site.register(RTKData)

f186c0af917604a410153ac7b1b5dd7bff5c3625
karasinga <mainpita@gmail.com>
2024-03-18 07:14:08 +0300

    Enhanced Admin Site Search and Cached Results Rendering

--- apps/cqi/admin.py
+++ apps/cqi/admin.py
@@ -12,10 +12,11 @@ class QI_ProjectsAdmin(admin.ModelAdmin):
     class by setting form = QI_ProjectsForm. Once you have created this custom form, you can register your custom
     admin class instead of the model class like this: admin.site.register( QI_Projects, QI_ProjectsAdmin). """
     form = QI_ProjectsForm
+    search_fields = ("facility_name__name", "facility_name__mfl_code")
 
 
 # admin.site.register(QI_Projects, QI_ProjectsAdmin)
-admin.site.register(QI_Projects)
+admin.site.register(QI_Projects,QI_ProjectsAdmin)
 admin.site.register(TestedChange)
 admin.site.register(ProjectComments)
 admin.site.register(ProjectResponses)

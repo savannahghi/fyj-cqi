73c0b7840011686d98592ab0b7a5dcf9443814c7
karasinga <mainpita@gmail.com>
2024-04-25 06:47:40 +0300

    load DATIM data dynamically

--- apps/dqa/admin.py
+++ apps/dqa/admin.py
@@ -4,10 +4,15 @@ from django.contrib import admin
 from apps.dqa.models import Period, Indicators, DataVerification, FyjPerformance, DQAWorkPlan, SystemAssessment, \
     AuditTeam, KhisPerformance, UpdateButtonSettings
 
+
+class FyjPerformanceAdmin(admin.ModelAdmin):
+    search_fields = ("facility", "mfl_code", "quarter_year", "month")
+
+
 admin.site.register(Period)
 admin.site.register(Indicators)
 admin.site.register(DataVerification)
-admin.site.register(FyjPerformance)
+admin.site.register(FyjPerformance, FyjPerformanceAdmin)
 admin.site.register(DQAWorkPlan)
 admin.site.register(SystemAssessment)
 admin.site.register(AuditTeam)

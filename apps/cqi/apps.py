09b8d83079ed63dad78bc152b37e7cb7605dc1df
karasinga <mainpita@gmail.com>
2024-04-29 15:11:58 +0300

    Send email to QI creator and stakeholders

--- apps/cqi/apps.py
+++ apps/cqi/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.cqi'
+
+    def ready(self):
+        import apps.cqi.signals

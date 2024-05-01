5388af74ea0ccb93102ecbbc4c72af636c2d8daa
saladgg <saladguyo60@gmail.com>
2024-05-01 10:27:49 +0300

    fix: set default values for email_host_user and email_host_password envars

--- config/settings/base.py
+++ config/settings/base.py
@@ -251,8 +251,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'FYJ CQI'
-EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default=None)
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default=None)
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 

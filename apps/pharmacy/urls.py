c52f78c4c4cafd5dadc0253ebefed5af18a9cadf
karasinga <76413541+karasinga@users.noreply.github.com>
2024-05-22 15:37:12 +0300

    add pharmacy dqa facility summary (#292)

--- apps/pharmacy/urls.py
+++ apps/pharmacy/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.pharmacy.views import add_pharmacy_records, add_inventory, show_work_plan, \
+from apps.pharmacy.views import add_pharmacy_records, add_inventory, dqa_dashboard, show_work_plan, \
     choose_facilities_inventory, choose_facilities_pharmacy, show_inventory, update_inventory, show_commodity_records, \
     update_pharmacy_records, create_inventory_work_plans, create_commodity_work_plans, update_workplan, \
     add_audit_team_pharmacy, show_audit_team_pharmacy, update_audit_team_pharmacy
@@ -12,7 +12,7 @@ urlpatterns = [
     path('facility-pharmacy/', choose_facilities_pharmacy, name="choose_facilities_pharmacy"),
     path('inventory/<str:report_name>/<str:quarter>/<str:year>/<uuid:pk>/<str:date>/', add_inventory,
          name="add_inventory"),
-    path('work-plan/', show_work_plan, name="show_work_plan"),
+    path('work-plan/', show_work_plan, name="pharmacy_show_work_plan"),
     path('create-inventory-work-plan/<uuid:pk>/<str:report_name>/', create_inventory_work_plans,
          name="create_work_plan"),
     path('create-commodity-work-plan/<uuid:pk>/', create_commodity_work_plans, name="create_commodity_work_plans"),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('add-audit-team/<uuid:pk>/<str:quarter_year>', add_audit_team_pharmacy, name="add_pharmacy_audit_team"),
     path('audit-team/', show_audit_team_pharmacy, name='show_audit_team_pharmacy'),
     path('update-audit-team/<uuid:pk>', update_audit_team_pharmacy, name="update_audit_team_pharmacy"),
+    path('pharmacy-dqa-dashboard/<str:dqa_type>', dqa_dashboard, name='pharmacy_dqa_dashboard'),
 ]
